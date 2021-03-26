3b68f426f22687183d0462117e06260e0f7cbdce
Dare Victor Adebayo <adebayovicktor@gmail.com>
2021-03-26 10:14:55 +0100

    updated dependencies

--- PortalApplication.API/Startup.cs
+++ PortalApplication.API/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using PortalApplication.Infrastructure.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,6 +29,7 @@ namespace PortalApplication.API
         {
 
             services.AddControllers();
+            services.ResolveInfrastructureServices(Configuration);
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PortalApplication.API", Version = "v1" });
