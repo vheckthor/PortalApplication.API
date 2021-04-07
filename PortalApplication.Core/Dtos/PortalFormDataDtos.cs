c4a1810c170d7d66d2f31e206ec0db94d130940b
victor Adebayo <adebayovicktor@gmail.com>
2021-04-08 00:02:18 +0100

    Added portal form development model classes

---
+++ PortalApplication.Core/Dtos/PortalFormDataDtos.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PortalApplication.Core.Dtos
+{
+     public class PortalFormDataDtos
+     {
+        [Required]
+        [StringLength(100)]
+        public string Fullname { get; set; }
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public DateTime DOB { get; set; }
+        [Required]
+        public string Role { get; set; }
+        [Required]
+        public string TetInstitution { get; set; }
+        [Required]
+        public string ClassOfDegree { get; set; }
+        [Required]
+        public int YearofExperience { get; set; }
+        [Required]
+        public string CurrentRole { get; set; }
+        [Required]
+        public string CurrentEmployer { get; set; }
+        
+        public string ProfessionalCertificate { get; set; }
+        [Required]
+        public string WhyRole { get; set; }
+        [Required]
+        public List<string> RelevantSkills { get; set; }
+        [Required]
+        public IFormFile CV { get; set; }
+     }
+}
---
+++ PortalApplication.Core/Models/PortalFormData.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+//using Microsoft.AspNetCore.Http;
+
+namespace PortalApplication.Core.Models
+{
+    public class PortalFormData
+    {
+        public int Id { get; set; }
+        public Guid UniqueIdentifier { get; set; }
+        public string Fullname { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public DateTime DOB { get; set; }
+        public string Role { get; set; }
+        public string TetInstitution { get; set; }
+        public string ClassOfDegree { get; set; }
+        public string YearofExperience { get; set; }
+        public string CurrentRole { get; set; }
+        public string CurrentEmployer { get; set; }
+        public string ProfessionalCertificate { get; set; }
+        public string WhyRole { get; set; }
+        public string RelevantSkills { get; set; }
+        public IFormFile CV { get; set; }
+        public DateTime DateCreated { get; set; }
+
+        public PortalFormData()
+        {
+            DateCreated = DateTime.Now;
+        }
+    }
+    
+}
