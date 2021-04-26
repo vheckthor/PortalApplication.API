426cb0a5c686fd215d48205d422926d79959d4e1
victor Adebayo <adebayovicktor@gmail.com>
2021-04-26 10:26:22 +0100

    Edited descriptions and Requirements

--- PortalApplication.API/Controllers/VacancyController.cs
+++ PortalApplication.API/Controllers/VacancyController.cs
@@ -1,6 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PortalApplication.Core.Dtos;
+using PortalApplication.Core.Models;
 using PortalApplication.Core.Services.Interfaces;
 using PortalApplication.Infrastructure.DataAccess;
 
@@ -19,13 +23,37 @@ namespace PortalApplication.API.Controllers
         
         public async Task<IActionResult> CreateVacancy(VacancyDto data)
         {
+            var listOfDescription = new List<Descriptions>();
+            var listOfRequirements = new List<Requirements>();
+            var vacancyUniqueId = Guid.NewGuid();
+            if(data.Requirement.Length !=0 || data.Description.Length != 0)
+            {
+                foreach(var item in data.Description)
+                {
+                    listOfDescription.Add(new Descriptions
+                    {
+                        JobDescriptions = item,
+                        VacancyUniqueId = vacancyUniqueId
+                    });
+                }
+
+                foreach(var item in data.Requirement)
+                {
+                    listOfRequirements.Add(new Requirements
+                    {
+                        JobRequirements = item,
+                        VacancyUniqueId = vacancyUniqueId
+                    });
+                }
+            }
             var sent = await _repo.CreateVacancy(new Core.Models.VacancyModel
                         {
                             JobId = data.JobId,
                             CompanyName = data.CompanyName,
                             Position = data.Position,
-                            Description = data.Description,
-                            Requirement = data.Requirement
+                            Requirement = listOfRequirements,
+                            Description = listOfDescription,
+                            VacancyUniqueId = vacancyUniqueId
                         });
             if (sent)
             {
